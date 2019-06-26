d3a20499624e7b45cc1be4270534fb4f7331fcc4
Ashbin Thapa <thapasmit82@gmail.com>
2019-06-26 15:34:13 +0545

    created sidebar and breaker widget

--- wp-content/themes/supermag-child/functions.php
+++ wp-content/themes/supermag-child/functions.php
@@ -22,3 +22,9 @@ endif;
 add_action( 'wp_enqueue_scripts', 'chld_thm_cfg_parent_css', 10 );
 
 // END ENQUEUE PARENT ACTION
+
+/*
+* Including sidebars and custom widgets
+*/
+
+require_once(get_stylesheet_directory() . '/sidebar-widgets/sidebar.php');
---
+++ wp-content/themes/supermag-child/sidebar-widgets/sidebar.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Register widget area.
+ *
+ * @link http://codex.wordpress.org/Function_Reference/register_sidebar
+ */
+ 
+function supermag_widget_init_child(){
+
+    register_sidebar(array(
+        'name' => __('Breaker News Sidebar Area', 'supermag'),
+        'id'   => 'supermag-sidebar-breaker',
+        'description' => __('Displays news on breaker section sidebar.', 'supermag'),
+        'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+        'after_widget' => '</aside>',
+        'before_title' => '<h3 class="widget-title"><span>',
+        'after_title' => '</span></h3>',
+    ));
+}
+add_action('widgets_init', 'supermag_widget_init_child');
